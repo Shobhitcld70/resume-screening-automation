ac3e4c5dbe764fb4355c95fd3620d4df3eb32870
Shobhitcld70 <krishnanshobhit@gmail.com>
2025-02-27 22:45:39 +0530

    Update sheets_utils.py

--- sheets_utils.py
+++ sheets_utils.py
@@ -4,7 +4,7 @@ import sys
 # Fix for Unicode issues in Windows
 sys.stdout.reconfigure(encoding='utf-8')
 
-WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwVmpbtC97l739ftFwOrarlnrQ7B0BnsF3kZdyOE7JbxVMz_oMl0_-0wfMUFe1bn_0HxQ/exec"
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzOb6QSNKUskCL_8zayo8H7b3ObltM5kXaHKA4PVwMl7QArOEZ1YMW2T3JZNUtrFYSUfA/exec"
 
 def send_to_google_sheets(name, email, phone, sentiment, summary):
     """Send processed resume data to Google Sheets via Apps Script"""
